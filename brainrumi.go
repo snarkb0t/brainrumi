f34b6b1ffe4f750f5589ca017ba559d4dda10376
snarkb0t <dirtychihuahua4@gmail.com>
2026-03-12 01:23:11 +0800

    initial commit

---
+++ brainrumi.go
@@ -0,0 +1,119 @@
+/*
+brainrumi - a brainfuck interpreter written in go by snarkb0t on github
+*/
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var (
+	raw bool
+)
+
+func eval(code string, cells *[]int32, p *int) {
+	var (
+		scopes map[string]int = make(map[string]int, 0)
+
+		open []int = make([]int, 0)
+	)
+
+	for s := 0; s < len(code); s++ {
+		switch code[s] {
+		case '[':
+			open = append(open, s)
+		case ']':
+			scopes[strconv.Itoa(open[len(open)-1])] = s
+			scopes[strconv.Itoa(s)] = open[len(open)-1]
+			open = open[:len(open)-1]
+		}
+	}
+
+	open = nil
+
+	for i := 0; i < len(code); i++ {
+		switch code[i] {
+		case '>':
+			if *p += 1; *p > len(*cells)-1 {
+				*cells = append(*cells, 0)
+			}
+		case '<':
+			if *p == 0 {
+				continue
+			}
+
+			*p -= 1
+		case '+':
+			(*cells)[*p] += 1
+		case '-':
+			(*cells)[*p] -= 1
+		case '.':
+			if raw {
+				fmt.Print((*cells)[*p])
+			} else {
+				fmt.Print(string((*cells)[*p]))
+			}
+		case ',':
+			var putInto string
+			fmt.Scanln(&putInto)
+			(*cells)[*p] = int32([]byte(putInto)[0])
+		case '[':
+			if (*cells)[*p] == 0 {
+				i = scopes[strconv.Itoa(i)]
+			}
+		case ']':
+			if (*cells)[*p] != 0 {
+				i = scopes[strconv.Itoa(i)]
+			}
+		}
+	}
+
+	fmt.Println()
+}
+
+func main() {
+	var (
+		cells []int32 = make([]int32, 1)
+		p     int     = 0
+	)
+
+	flag.BoolVar(&raw, "r", false, "display raw cell data instead of encoded text")
+	flag.Parse()
+
+	if len(flag.Args()) > 0 {
+
+		file, err := os.ReadFile(flag.Arg(0))
+
+		if err != nil {
+			panic("file not found")
+		} else {
+			eval(string(file), &cells, &p)
+			return
+		}
+	}
+
+	fmt.Println("brainrumi || made by snarkb0t on github.")
+	fmt.Println("enter 'q', 'quit' or 'exit' to exit. enter 'r' to change display mode (encoded text / raw cell data)")
+	fmt.Println()
+
+	for {
+		var line string
+
+		fmt.Print(">>> ")
+		fmt.Scan(&line)
+
+		if line == "q" || line == "quit" || line == "exit" {
+			return
+		}
+
+		if line == "r" {
+			raw = !raw
+		}
+
+		eval(line, &cells, &p)
+	}
+}
